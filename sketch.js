b10b2a7232ff466a0d21c07361fb18bcfacc4ec0
Prasadnaw <79042630+Prasadnaw@users.noreply.github.com>
2021-05-18 21:30:22 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(DisplayWidth -16 ,DisplayHeight - 110);
+  createCanvas(displayWidth - 15 ,displayHeight - 110);
 
   playerShip = createSprite(displayWidth/2, displayHeight - 150 , 50, 50);
   playerShip.addImage(playerShipSprite);
